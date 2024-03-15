81a6247b1f12b498989a8df0ca662d520793b3b9
Andrija Kostic <kostic.andrija00@gmail.com>
2024-03-15 08:12:45 +0100

    Add new font styles

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -14,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: "Inter", sans-serif;
 }
 
 body {
@@ -24,37 +24,72 @@ body {
 .style-h1 {
     font-size: 48px;
     font-weight: 600;
-    line-height: 62px;
 }
 
 .style-h2 {
     font-size: 20px;
     font-weight: 500;
-    line-height: 30px;
 }
 
 .style-h3 {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1;
+}
+
+.style-h4 {
     font-size: 16px;
     font-weight: 500;
-    line-height: 24px;
+    line-height: 1;
 }
 
-.style-desc {
+.style-h5 {
     font-size: 14px;
-    font-weight: 400;
-    line-height: 21px;
+    font-weight: 500;
+    line-height: 1.5;
 }
 
-.style-p {
+.style-h6 {
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 1;
+    letter-spacing: 0.2px;
+}
+
+.style-body {
     font-size: 16px;
     font-weight: 400;
-    line-height: 24px;
+    line-height: 1.5;
+}
+
+
+.style-body-link {
+    font-size: 16px;
+    font-weight: 600;
+}
+
+.style-small {
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.6;
 }
 
 .style-button {
-    font-size: 12px;
+    font-size: 18px;
     font-weight: 400;
-    line-height: 18px;
+}
+
+.style-chip {
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 1;
+}
+
+.style-nav__link {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1;
+    letter-spacing: 1px;
 }
 `;
 export default GlobalStyle;
