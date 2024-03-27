cf31c792d7af22681e64be4372de32892b4a5c48
Andrija Kostic <kostic.andrija00@gmail.com>
2024-03-27 22:09:52 +0100

    Add 3D rotate card effect

--- src/components/HoverWrapper/HoverWrapper.jsx
+++ src/components/HoverWrapper/HoverWrapper.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 
 const StyledWrapper = styled.a`
+  --rX: 0;
+  --rY: 0;
   text-decoration: none;
   position: relative;
   z-index: 1;
-  transition: opacity 250ms ease-in;
+  transform-style: preserve-3d;
+  transform: perspective(1000px) rotateX(var(--rX)) rotateY(var(--rY));
+  transition: all 250ms ease;
 
   &.not-focused {
     opacity: 0.4;
@@ -48,17 +52,48 @@ const HoverWrapper = ({
   index,
   highlightedIndex,
   children,
-}) => (
-  <StyledWrapper
-    onMouseEnter={() => handleMouseEnter(index)}
-    onMouseLeave={() => handleMouseLeave()}
-    href={href}
-    className={
-      highlightedIndex !== null && highlightedIndex !== index && "not-focused"
-    }
-  >
-    {children}
-  </StyledWrapper>
-);
+}) => {
+  const itemRef = useRef();
+
+  const handleMouseMove = (e) => {
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    const { width, height } = itemRef.current.getBoundingClientRect();
+    const halfWidth = width / 2;
+    const halfHeight = height / 2;
+
+    const sensitivity = 5;
+    const maxRotation = 20;
+    let rotationX = ((y - halfHeight) / halfHeight) * sensitivity;
+    let rotationY = ((x - halfWidth) / halfWidth) * sensitivity;
+
+    rotationX = Math.max(-maxRotation, Math.min(maxRotation, rotationX));
+    rotationY = Math.max(-maxRotation, Math.min(maxRotation, rotationY));
+
+    itemRef.current.style.setProperty("--rX", `${rotationX}deg`);
+    itemRef.current.style.setProperty("--rY", `${rotationY}deg`);
+  };
+
+  const handleOnMouseLeave = () => {
+    itemRef.current.style.setProperty("--rX", "0deg");
+    itemRef.current.style.setProperty("--rY", "0deg");
+    handleMouseLeave();
+  };
+
+  return (
+    <StyledWrapper
+      ref={itemRef}
+      onMouseEnter={() => handleMouseEnter(index)}
+      onMouseMove={(e) => handleMouseMove(e)}
+      onMouseLeave={(e) => handleOnMouseLeave(e)}
+      href={href}
+      className={
+        highlightedIndex !== null && highlightedIndex !== index && "not-focused"
+      }
+    >
+      {children}
+    </StyledWrapper>
+  );
+};
 
 export default HoverWrapper;
